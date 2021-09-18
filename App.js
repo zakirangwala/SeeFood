4ff58d412ad819051b9f6cb6c5b73adedf66fd67
albertlai431 <albertlai631@outlook.com>
2021-09-18 15:40:06 -0400

    add firebase

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import firebase from 'firebase/app';
+
+//firebase services
+//import "firebase/auth";
+//import "firebase/database";
+// import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: 'api-key',
+//   authDomain: 'seefood-3cd7b.firebaseapp.com',
+//   databaseURL: 'https://project-id.firebaseio.com',
+//   projectId: 'seefood-3cd7b',
+//   storageBucket: 'seefood-3cd7b.appspot.com',
+//   messagingSenderId: 'sender-id',
+//   appId: 'app-id',
+//   measurementId: 'G-measurement-id',
+// };
+
+//firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
