003965437f41297ad88046b6697d1ac31ef46691
albertlai431 <albertlai631@outlook.com>
2021-09-18 16:27:58 -0400

    add camera functionality

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Entypo } from '@expo/vector-icons';
 //import firebase from 'firebase/app';
 
 //firebase services
@@ -24,10 +26,42 @@ import { StyleSheet, Text, View } from 'react-native';
 //firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  //camera permissions
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  //check permsissions
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+      </Camera>
+      <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+             <Entypo name="circle" size={72} color="white" />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -35,8 +69,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black',
   },
+  camera: {
+    width: '100%',
+    height: '70%',
+  },
+  buttonContainer:{
+    marginTop: 40,
+  },
+  button:{},
+  text:{}
 });
