4103f0e9e175cfeece85d9d78b505e8db89a99b2
albertlai431 <albertlai631@outlook.com>
2021-09-19 09:54:05 -0400

    settings screen somewhat working

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,100 +1,110 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import vegan from '../../assets/vegan.png'; 
-import vegetarian from '../../assets/plant-based.png'; 
+import vegan from '../../assets/vegan.png';
+import vegetarian from '../../assets/plant-based.png';
 import glutenFree from '../../assets/gluten-free.png';
-import nonVegan from '../../assets/unclicked-vegan.png'; 
-import nonVegetarian from '../../assets/unclicked-plant-based.png'; 
+import nonVegan from '../../assets/unclicked-vegan.png';
+import nonVegetarian from '../../assets/unclicked-plant-based.png';
 import nonGlutenFree from '../../assets/unclicked-gluten-free.png';
 
-export default function SettingsScreen({navigation}){
+export default function SettingsScreen({ navigation }) {
 
-    const [restrictions, setRestrictions] = useState([]);
+    let [restrictions, setRestrictions] = useState('');
     //const [extra, setExtra] = useState([]);
 
     useEffect(() => {
         setRestrictions(getData('restrictions'));
         //setExtra(getData('extra'));
-    });
-    
-    const getData = async (key) => {
+    }, []);
+
+
+    useEffect(() => {
+        console.log(restrictions);
+        setData('restrictions', restrictions);
+    }, [restrictions]);
+
+
+    const getData = (key) => {
         try {
-            const jsonValue = await AsyncStorage.getItem(key);
+            const jsonValue = AsyncStorage.getItem(key);
             return jsonValue != null ? JSON.parse(jsonValue) : null;
-        } catch(e) {
-            console.log('hey');
-            setData(key,[]);
+        } catch (e) {
+            setData(key, []);
             return [];
         }
     }
-      
+
     const setData = async (key, value) => {
         try {
             const jsonValue = JSON.stringify(value)
             await AsyncStorage.setItem(key, jsonValue)
-            setState(()=>{});
         } catch (e) {
-        // saving error
+            // saving error
         }
     }
 
-    console.log(restrictions);
-
-    const buttonOnPress = (key, arr, value, add) => {
-        console.log(arr);
-        if(add){
-            let arrNew = arr.push(value);
-            setData(key,arrNew);
+    const buttonOnPress = (value, add) => {
+        if (add) {
+            setRestrictions(restrictions.concat([value]));
         }
-        else{
-            let arrNew = arr.splice(arr.indexOf(value), 1)
-            setData(key,arrNew);
+        else {
+            setRestrictions(restrictions.filter(val => val != value));
         }
     }
 
-    return(
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Choose Your Diet:
-        </Text>
-        <View style={styles.row}>
-            {(restrictions == null || !(restrictions instanceof Array)) ? <View></View> : ((restrictions.includes("vegan")) ? 
-                <TouchableOpacity onPress={buttonOnPress('restrictions',restrictions,'vegan',0)}><Image source={vegan} style={{ width: 80, height: 80 }}/></TouchableOpacity> : 
-                <TouchableOpacity onPress={buttonOnPress('restrictions',restrictions,'vegan',1)}><Image source={nonVegan} style={{ width: 80, height: 80 }}/></TouchableOpacity>)}
-            
+    return (
+        (restrictions != null && restrictions instanceof Array) && <View style={styles.container}>
+            <Text style={styles.title}>
+                Choose Your Diet:
+            </Text>
+            <View style={styles.row}>
+                {((restrictions.includes("vegan")) ?
+                    <TouchableOpacity onPress={() => buttonOnPress('vegan', 0)}><Image source={vegan} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                    <TouchableOpacity onPress={() => buttonOnPress('vegan', 1)}><Image source={nonVegan} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                <View style={styles.gap}></View>
+                {((restrictions.includes("vegetarian")) ?
+                    <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 0)}><Image source={vegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                    <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 1)}><Image source={nonVegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                {((restrictions.includes("glutenFree")) ?
+                    <TouchableOpacity onPress={() => buttonOnPress('glutenFree', 0)}><Image source={glutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                    <TouchableOpacity onPress={() => buttonOnPress('glutenFree', 1)}><Image source={nonGlutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+
+            </View>
+            <Text style={styles.title}>
+                Extra Dietary Restrictions:
+            </Text>
         </View>
-      </View>
     );
-  }
+}
 
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      paddingHorizontal: 35,
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      backgroundColor: '#ffffff',
+        flex: 1,
+        paddingHorizontal: 35,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        backgroundColor: '#ffffff',
     },
     title: {
         fontSize: 34,
         fontWeight: 'bold',
         color: 'black',
-        paddingVertical:30
-      },
-      row: {
-        flexDirection: 'row', 
-        justifyContent: "space-between", 
-        alignItems:"center",
+        paddingVertical: 30
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center",
         paddingTop: 30,
         paddingBottom: 30,
         marginHorizontal: 10
     },
     paragraph: {
-      margin: 24,
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      color: '#34495e',
-    },
-  });
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    }
+});
