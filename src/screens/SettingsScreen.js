ffeb9a4ec3798ccfebdccb82182fc21f1f3d296c
albertlai431 <albertlai631@outlook.com>
2021-09-19 10:48:02 -0400

    finish settings

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -11,9 +11,8 @@ import nonGlutenFree from '../../assets/unclicked-gluten-free.png';
 export default function SettingsScreen({ navigation }) {
 
     let [restrictions, setRestrictions] = useState('');
+    let [lastTapped, setLastTapped] = useState('vegan');
     //const [extra, setExtra] = useState([]);
-
-    let lastTapped = "vegan";
 
     useEffect(() => {
         setRestrictions(getData('restrictions'));
@@ -47,11 +46,11 @@ export default function SettingsScreen({ navigation }) {
 
     const buttonOnPress = (value, add) => {
         if (add) {
-            lastTapped = value;
+            setLastTapped(value);
             setRestrictions(restrictions.concat([value]));
         }
         else {
-            lastTapped = value;
+            setLastTapped(value);
             setRestrictions(restrictions.filter(val => val != value));
         }
     }
@@ -62,26 +61,34 @@ export default function SettingsScreen({ navigation }) {
                 Choose Your Diet:
             </Text>
             <View style={styles.row}>
-                {((restrictions.includes("vegan")) ?
-                    <TouchableOpacity onPress={() => buttonOnPress('vegan', 0)}><Image source={vegan} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
-                    <TouchableOpacity onPress={() => buttonOnPress('vegan', 1)}><Image source={nonVegan} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
-                <View style={styles.gap}></View>
-                {((restrictions.includes("vegetarian")) ?
-                    <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 0)}><Image source={vegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
-                    <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 1)}><Image source={nonVegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
-                {((restrictions.includes("glutenFree")) ?
-                    <TouchableOpacity onPress={() => buttonOnPress('glutenFree', 0)}><Image source={glutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
-                    <TouchableOpacity onPress={() => buttonOnPress('glutenFree', 1)}><Image source={nonGlutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                <View style={styles.column}>
+                    <Text style={styles.heading}>Vegan</Text>
+                    <View style={styles.gap}></View>
+                    {((restrictions.includes("vegan")) ?
+                        <TouchableOpacity onPress={() => buttonOnPress('vegan', 0)}><Image source={vegan} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                        <TouchableOpacity onPress={() => buttonOnPress('vegan', 1)}><Image source={nonVegan} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                </View>
+                <View style={styles.column}>
+                    <Text style={styles.heading}>Vegetarian</Text>
+                    <View style={styles.gap}></View>
+                    {((restrictions.includes("vegetarian")) ?
+                        <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 0)}><Image source={vegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                        <TouchableOpacity onPress={() => buttonOnPress('vegetarian', 1)}><Image source={nonVegetarian} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                </View>
+                <View style={styles.column}>
+                    <Text style={styles.heading}>Gluten-Free</Text>
+                    <View style={styles.gap}></View>
+                    {((restrictions.includes("gluten Free")) ?
+                        <TouchableOpacity onPress={() => buttonOnPress('gluten Free', 0)}><Image source={glutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity> :
+                        <TouchableOpacity onPress={() => buttonOnPress('gluten Free', 1)}><Image source={nonGlutenFree} style={{ width: 80, height: 80 }} /></TouchableOpacity>)}
+                </View>
             </View>
-            {/* <View style={styles.desc}>
-                <Text style={styles.paragraph}>Description</Text>
+            <View style={styles.desc}>
+                <Text style={styles.paragraph}>{lastTapped.charAt(0).toUpperCase() + lastTapped.slice(1)}: Description</Text>
                 {(lastTapped === 'vegan') ? <Text>A vegan diet involves eating only foods comprising plants. Those who follow this diet avoid all animal products, including meat, dairy, and eggs.</Text> :
                     ((lastTapped == 'vegetarian') ? <Text>The vegetarian diet involves abstaining from eating meat, fish and poultry, but may continue to eat eggs and dairy products.</Text> :
                         <Text>A gluten-free diet is an eating plan that excludes foods containing gluten, a protein found in wheat, barley, rye and triticale.</Text>)}
             </View>
-            <Text style={styles.title}>
-                Extra Dietary Restrictions:
-            </Text> */}
         </View>
     );
 }
@@ -93,29 +100,40 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         backgroundColor: '#ffffff',
     },
+    heading: {
+        fontSize: 18,
+        color: 'black',
+        fontWeight: 'bold',
+    },
     title: {
         fontSize: 34,
         fontWeight: 'bold',
         color: 'black',
-        paddingVertical: 30
+        paddingTop: 30,
+        paddingBottom: 15
     },
     row: {
         flexDirection: 'row',
         justifyContent: "space-between",
         alignItems: "center",
-        paddingTop: 30,
         paddingBottom: 30,
-        marginHorizontal: 10
     },
     paragraph: {
-        margin: 24,
         fontSize: 18,
+        marginBottom: 10,
         fontWeight: 'bold',
-        textAlign: 'center',
         color: 'black',
     },
     desc: {
         borderRadius: 5,
-        borderWidth: 1
-    }
+        borderWidth: 1,
+        padding: 10,
+    },
+    column: {
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    gap: {
+        height: 10,
+    },
 });
