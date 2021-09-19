842479d8a88073e459153d1fba83a6e092b2dc7f
albertlai431 <albertlai631@outlook.com>
2021-09-19 04:14:39 -0400

    settings screen but doesn't work

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,13 +1,69 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import vegan from '../../assets/vegan.png'; 
+import vegetarian from '../../assets/plant-based.png'; 
+import glutenFree from '../../assets/gluten-free.png';
+import nonVegan from '../../assets/unclicked-vegan.png'; 
+import nonVegetarian from '../../assets/unclicked-plant-based.png'; 
+import nonGlutenFree from '../../assets/unclicked-gluten-free.png';
 
 export default function SettingsScreen({navigation}){
+
+    const [restrictions, setRestrictions] = useState([]);
+    //const [extra, setExtra] = useState([]);
+
+    useEffect(() => {
+        setRestrictions(getData('restrictions'));
+        //setExtra(getData('extra'));
+    });
+    
+    const getData = async (key) => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(key);
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch(e) {
+            console.log('hey');
+            setData(key,[]);
+            return [];
+        }
+    }
+      
+    const setData = async (key, value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem(key, jsonValue)
+            setState(()=>{});
+        } catch (e) {
+        // saving error
+        }
+    }
+
+    console.log(restrictions);
+
+    const buttonOnPress = (key, arr, value, add) => {
+        console.log(arr);
+        if(add){
+            let arrNew = arr.push(value);
+            setData(key,arrNew);
+        }
+        else{
+            let arrNew = arr.splice(arr.indexOf(value), 1)
+            setData(key,arrNew);
+        }
+    }
+
     return(
       <View style={styles.container}>
-        <Text style={{fontWeight: 'bold'}}>
+        <Text style={styles.title}>
           Choose Your Diet:
         </Text>
+        <View style={styles.row}>
+            {(restrictions == null || !(restrictions instanceof Array)) ? <View></View> : ((restrictions.includes("vegan")) ? 
+                <TouchableOpacity onPress={buttonOnPress('restrictions',restrictions,'vegan',0)}><Image source={vegan} style={{ width: 80, height: 80 }}/></TouchableOpacity> : 
+                <TouchableOpacity onPress={buttonOnPress('restrictions',restrictions,'vegan',1)}><Image source={nonVegan} style={{ width: 80, height: 80 }}/></TouchableOpacity>)}
+            
+        </View>
       </View>
     );
   }
@@ -15,9 +71,24 @@ export default function SettingsScreen({navigation}){
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      paddingHorizontal: 35,
       alignItems: 'flex-start',
       justifyContent: 'flex-start',
       backgroundColor: '#ffffff',
+    },
+    title: {
+        fontSize: 34,
+        fontWeight: 'bold',
+        color: 'black',
+        paddingVertical:30
+      },
+      row: {
+        flexDirection: 'row', 
+        justifyContent: "space-between", 
+        alignItems:"center",
+        paddingTop: 30,
+        paddingBottom: 30,
+        marginHorizontal: 10
     },
     paragraph: {
       margin: 24,
