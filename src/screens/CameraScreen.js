8031b403880719d10c93adae027fe057456ce7bb
albertlai431 <albertlai631@outlook.com>
2021-09-18 22:40:51 -0400

    bottom bar nav

---
+++ src/screens/CameraScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ScannerScreen from './ScannerScreen';
+import ResultsScreen from './ResultsScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function CameraScreen(){
+    return(
+        <Stack.Navigator initialRouteName="Scanner" 
+        // screenOptions={{
+        //     headerShown: false
+        //   }}
+          >
+            <Stack.Screen name="Scanner" component={ScannerScreen} />
+            <Stack.Screen name="Results" component={ResultsScreen} />
+        </Stack.Navigator> 
+    );
+}
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,4 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function SettingsScreen({navigation}){
-    
+    return(
+        <View></View>
+    )
   }
